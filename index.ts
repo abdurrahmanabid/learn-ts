81244da0d37e2a842625ea34e90df2027ec75bc4
abdurrahmanabid <muhaabdurrahman@gmail.com>
2025-04-13 23:31:34 +0600

    refactor: update comments for function and array sections in index files

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
-// Function 
+//! Function 
 // function multiply(a : number,b : number){
 //     return a * b;
 // }
 // console.log(multiply(39,3)); 
+
+//! array 
+let arr = ['banana', 'apple', 'orange'];
+arr.push(32); // Error: Argument of type 'number' is not assignable to parameter of type 'string'.

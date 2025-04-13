d591da5fd53c5722f010b3a7ffc18b4e2f6fa89f
abdurrahmanabid <muhaabdurrahman@gmail.com>
2025-04-13 23:26:36 +0600

    initialize project with HTML, JavaScript, TypeScript files and TypeScript configuration

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+// Function 
+// function multiply(a : number,b : number){
+//     return a * b;
+// }
+// console.log(multiply(39,3)); 
---
+++ index.ts
@@ -0,0 +1,5 @@
+// Function 
+// function multiply(a : number,b : number){
+//     return a * b;
+// }
+// console.log(multiply(39,3)); 

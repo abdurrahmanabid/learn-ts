537b526dacfafff446bff9dfb40f58253e0b22d5
abdurrahmanabid <muhaabdurrahman@gmail.com>
2025-04-13 23:33:57 +0600

    refactor: comment out array and object examples in index files

--- index.js
+++ index.js
@@ -5,5 +5,12 @@
 // }
 // console.log(multiply(39,3)); 
 //! array 
-let arr = ['banana', 'apple', 'orange'];
-arr.push(32); // Error: Argument of type 'number' is not assignable to parameter of type 'string'.
+// let arr = ['banana', 'apple', 'orange'];
+// arr.push(32); // Error: Argument of type 'number' is not assignable to parameter of type 'string'.
+//! object
+// let obj = {
+//     name: 'John',
+//     age: 30,
+//     isStudent: false
+// };
+// obj.age = 'thirty'; // Error: Type 'string' is not assignable to type 'number'.
